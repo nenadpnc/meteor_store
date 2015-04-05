9d05b9b365e62cf834c2f9fa5f98d207b483b386
Nenad <nenadpnc@gmail.com>
2015-04-05 23:45:45 +0200

    seeding database

--- meteor_store/server/fixtures.js
+++ meteor_store/server/fixtures.js
@@ -21,25 +21,45 @@ var categories = [
     }
 ];
 
-var product = {
-    name: 'Black Porcelain Bowl',
-    manufacturer: 'Hasami Porcelain',
-    category: 'Home',
-    thumbSrc: 'images/thumbs/yes_dtldysi-443x500-430x480.jpg',
-    description: 'Made in Japan from unglazed porcelain with a texture and organic feel that is unique to Hasami. Every dish and oak lid stacks on top of each other which creates a very organized display. Oak lid not included. Designed by Takuhiro Shinomoto.',
-    sizes: [
-        { name: 'Small', inStock: 5, price: 24, materials: 'H:2.12" x W: 5.66" x D: 5.66"'},
-        { name: 'Medium', inStock: 5, price: 36, materials: 'H:2.12" x W: 7.33" x D: 7.33"'},
-        { name: 'Extra Large', inStock: 5, price: 85, materials: 'H:2.12" x W: 10" x D: 10"'},
-        { name: 'Extra Small', inStock: 5, price: 16, materials: 'H:2.87" x W: 3.33" x D: 3.33"'}
-    ],
-    year: '2010',
-    images: []
-};
+function randomIntFromInterval(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+var thumbsArray = [
+    'images/thumbs/yes_dtldysi-443x500-430x480.jpg',
+    'images/thumbs/yes_thumbs_aalto_vase-430x480.jpg',
+    'images/thumbs/yes_thumbs_black_bowl-443x500-430x4800.jpg',
+    'images/thumbs/yes_thumbs_helvetica_book-430x480.jpg',
+    'images/thumbs/yes_thumbs_sphrical_vase-430x480.jpg',
+    'images/thumbs/yes_thumbs_blockitecture-430x480.jpg',
+    'images/thumbs/yes_thumbs_boskee_cube-430x480.jpg',
+    'images/thumbs/yes_thumbs_cast_iron_birds-430x480.jpg',
+    'images/thumbs/yes_thumbs_hasami_lids-430x480.jpg',
+    'images/thumbs/yes_thumbs_Truck_Nest_Book-430x480.jpg',
+    'images/thumbs/yes_thumbs_yanagi_tea_kettle-430x480.jpg',
+    'images/thumbs/yes_thumbs_analog_alarm_clock-430x480.jpg'
+];
+
+var categoryArray = ['Home', 'Office', 'Children', 'Books & magazines', 'Outdoor'];
 
 if (Products.find().count() === 0) {
-    for (var j = 0; j < 23; j++) {
-        Products.insert(product);
+    for (var j = 0; j < 50; j++) {
+        Products.insert({
+            name: Fake.word(),
+            manufacturer: Fake.word(),
+            thumbSrc: Fake.fromArray(thumbsArray),
+            category: Fake.fromArray(categoryArray),
+            description: Fake.paragraph(3),
+            sizes: [
+                { name: 'Small', inStock: randomIntFromInterval(1, 25), price: randomIntFromInterval(15, 25), materials: 'H:2.12" x W: 5.66" x D: 5.66"'},
+                { name: 'Medium', inStock: randomIntFromInterval(1, 25), price: randomIntFromInterval(26, 67), materials: 'H:2.12" x W: 7.33" x D: 7.33"'},
+                { name: 'Extra Large', inStock: randomIntFromInterval(1, 25), price: randomIntFromInterval(68, 250), materials: 'H:2.12" x W: 10" x D: 10"'},
+                { name: 'Extra Small', inStock: randomIntFromInterval(1, 25), price: randomIntFromInterval(5, 15), materials: 'H:2.87" x W: 3.33" x D: 3.33"'}
+            ],
+            year: Fake.fromArray(['2010', '2011', '2012', '2013', '2014']),
+            images: []
+        });
     }
 }
 
